ff56002c98b2e8256879a51a8a618d558e1729b3
Mark K Cowan <mark@battlesnake.co.uk>
2015-06-01 17:12:00 +0300

    Smart watchers totally removed since they dont work, separate dumb-watch task instead

--- index.js
+++ index.js
@@ -25,7 +25,8 @@ patchLess();
 
 var gulp = require('gulp');
 
-var buildTasks = ['js', 'css', 'html'].concat(config.extraTasks || []);
+var buildTasks = ['js', 'css', 'html'].concat(config.extraBuildTasks || []);
+var watchTasks = [watchJs, watchJade, watchLess, watchWebfonts].concat(config.extraWatchTasks || []);
 
 gulp.task('default', ['test']);
 gulp.task('build', buildTasks);
@@ -33,7 +34,8 @@ gulp.task('rebuild', rebuildTask);
 gulp.task('html', ['jade']);
 gulp.task('css', ['less']);
 gulp.task('js', ['js-vendor', 'js-client']);
-gulp.task('test', ['rebuild', 'serve']);
+gulp.task('test', ['rebuild', 'serve', 'watch']);
+gulp.task('watch', watchTask);
 gulp.task('js-vendor', jsVendorTask);
 gulp.task('js-client', jsClientTask);
 gulp.task('jade', jadeTask);
@@ -45,7 +47,6 @@ gulp.task('serve', serveTask);
 gulp.task('clean', cleanTask);
 
 var live = isSet('live');
-var watching = !live;
 
 var path = require('path');
 var assign = require('lodash').assign;
@@ -79,10 +80,6 @@ var jade = require('gulp-jade');
 var fonts = require('gulp-google-webfonts');
 var concat = require('gulp-concat');
 var data = require('gulp-data');
-var watch = watching ? require('gulp-watch') : gulp.src.bind(gulp);
-var watchify = watching ? require('watchify') : function (brows) { return brows; };
-var watchLess = watching ? require('gulp-watch-less') : identity;
-var watchJade = watching ? require('gulp-jade-find-affected') : identity;
 
 function isSet(name) {
 	return ["", "0", "false", "no"].indexOf(process.env[name] || '') === -1;
@@ -125,14 +122,29 @@ function jsVendorTask() {
 		;
 }
 
-var jsClientWatch;
+function watchTask() {
+	watchTasks.forEach(function (fn) {
+		fn().on('error', errorReporter);
+	});
+}
+
+function watchJs() {
+	return gulp.watch(config.globs.jsClientDeps || [], ['js-client']);
+}
+
+function watchJade() {
+	return gulp.watch((config.globs.jadeDeps || []).concat(config.globs.jade), ['jade']);
+}
+
+function watchLess() {
+	return gulp.watch((config.globs.lessDeps || []).concat(config.globs.less), ['less']);
+}
+
+function watchWebfonts() {
+	return gulp.watch(config.fonts, ['webfonts']);
+}
+
 function jsClientTask() {
-	var toWatch = config.globs.jsClientDeps || [];
-	if (watching) {
-		jsClientWatch = jsClientWatch ||
-			gulp.watch(toWatch, ['js-client'])
-				.on('error', errorReporter);
-	}
 
 	/* Only minify client bundle if doing build for live */
 	var minifyifyConditional = live ? minifyify : function (a) { return a; };
@@ -142,7 +154,6 @@ function jsClientTask() {
 		debug: true,
 		fullPaths: true
 	};
-//	opts = assign(opts, watchify.args);
 
 	return new Browserify(opts)
 		.external(dependencies)
@@ -163,80 +174,23 @@ function jsClientTask() {
 		.pipe(sourcemaps.write('./'))
 		.pipe(gulp.dest(config.paths.out))
 		;
-
-	/* Client bundle configuration */
-//	var bundler = new Browserify(opts)
-//		.external(dependencies)
-//		.transform(envify)
-//		.transform(strictify)
-//		.transform(ngAnnotate)
-//		.transform(debowerify)
-//		.transform(deamdify)
-//		.plugin(minifyifyConditional, {
-//			map: config.bundles.clientMap,
-//			output: path.join(config.paths.out, config.bundles.clientMap)
-//		})
-//		;
-//
-//	bundler = watchify(bundler, { poll: 300, delay: 300 })
-//		.on('update', onUpdate)
-//		.on('time', onUpdateComplete)
-//		;
-//
-//	return onUpdate();
-//
-//	function onUpdate(ids) {
-//		util.log('Browserifying' + (ids ? ': ' + ids.map(function (s) { return chalk.cyan(s); }).join(', ') : '...'));
-//		return bundler
-//			.bundle()
-//			.on('error', onError)
-//			.pipe(source(config.bundles.client))
-//			.pipe(buffer())
-//			.pipe(sourcemaps.init({ loadMaps: true }))
-//			.pipe(sourcemaps.write('./'))
-//			.pipe(gulp.dest(config.paths.out))
-//			;
-//	}
-//
-//	function onUpdateComplete(time) {
-//		util.log(chalk.underline('Browserified in ' + chalk.magenta(time) + ' ms'));
-//	}
-//
-//	function onError(err) {
-//		errorReporter(err);
-//	}
 }
 
-var jadeWatch;
 function jadeTask() {
-	var toWatch = (config.globs.jadeDeps || []).concat(config.globs.jade);
-	if (watching) {
-		jadeWatch = jadeWatch ||
-			gulp.watch(toWatch, ['jade'])
-				.on('error', errorReporter);
-	}
 	var dataGetter =
 		(config.jadeContext instanceof Function) ? data(config.jadeContext) :
 		(config.jadeContext !== undefined) ? data(function (file) { return config.jadeContext; }) :
 		identity();
 	return gulp.src(config.globs.jade)
 		.pipe(errorHandler())
-//		.pipe(watchJade())
 		.pipe(dataGetter)
 		.pipe(jade({ pretty: !live }))
 		.pipe(gulp.dest(config.paths.out))
 		;
 }
 
-var lessWatch;
 function lessTask() {
 	var opts = { relativeUrls: true, strictUnits: true, strictMath: true };
-	var toWatch = (config.globs.lessDeps || []).concat(config.globs.less);
-	if (watching) {
-		lessWatch = lessWatch ||
-			gulp.watch(toWatch, ['less'])
-				.on('error', errorReporter);
-	}
 	var base64opts = {
 		debug: false,
 		extensions: config.base64.includeExtensions,
@@ -245,7 +199,6 @@ function lessTask() {
 	var prefixer = autoprefix({ browsers: ['last 2 versions'], cascade: false });
 	return gulp.src(config.globs.less)
 		.pipe(errorHandler())
-//		.pipe(watchLess(toWatch, { name: 'LESS', less: opts }))
 		.pipe(sourcemaps.init())
 		.pipe(less(opts))
 		.pipe(gif(config.base64.enabled, base64(base64opts)))
@@ -258,9 +211,6 @@ function lessTask() {
 }
 
 function webfontsTask() {
-	if (watching) {
-		gulp.watch(config.fonts, ['webfonts']);
-	}
 	return gulp.src(config.fonts)
 		.pipe(errorHandler())
 		.pipe(fonts())
